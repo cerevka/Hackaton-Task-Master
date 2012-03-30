7689ffbe6f99e2e5fe3262edcbbe3049bdabc61e
Petr Vales <Petr.V4les@gmail.com>
2012-03-31 00:53:08 +0200

    login controller

---
+++ src/java/hackaton/controller/LoginController.java
@@ -0,0 +1,22 @@
+package hackaton.controller;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import hackaton.model.DAO;
+import hackaton.model.User;
+
+public class LoginController {
+    
+    private final DAO dao;
+    
+    public LoginController(DAO dao) {
+        this.dao = dao;
+    }
+    
+    public User getUser() {
+        UserService userService = UserServiceFactory.getUserService();
+        com.google.appengine.api.users.User user = userService.getCurrentUser();
+        return dao.getUser(user.getEmail());
+    }
+    
+}
