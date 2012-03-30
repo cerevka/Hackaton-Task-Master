b17d11177bfa11c4519609c527e5cb68cb13335f
peckazde <peckazde@gmail.com>
2012-03-31 00:07:04 +0200

    implementace DAO

--- src/java/hackaton/model/DAO.java
+++ src/java/hackaton/model/DAO.java
@@ -1,5 +1,21 @@
 package hackaton.model;
 
+import java.util.List;
+
 public interface DAO {
-    
+
+    public List<Task> getMyTasks(Long userId);
+    public Task getTask(Long id);
+    public List<Task> getAllTasks();
+    public void newTask(Task task);
+    public void newUser(User user);
+    public List<User> getAllUsers();
+    public User getUser(Long id);
+    public List<Tag> getTagsForTaskByUser(Long taskId, Long userId);
+    public Ownership getOwnershipForTaskByUser(Long taskId, Long userId);
+    public void newTagToTask(Tag tag);
+    public List<Comment> getCommentToTask(Long taskId);
+    public void newComment(Comment comment);
+    public List<Ownership> getOwnershipByUser(Long userId);
+    public List<Ownership> getOwnershipByTaks(Long taskId);
 }
