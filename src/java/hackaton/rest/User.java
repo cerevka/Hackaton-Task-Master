8c505bda15e3db9124b6bf3a7ee38b8f174f15e7
Adam Činčura <cincura@atlas.cz>
2012-03-31 08:41:23 +0200

    editovatelný user

--- src/java/hackaton/rest/User.java
+++ src/java/hackaton/rest/User.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
@@ -19,7 +20,7 @@ public class User {
 
     @GET
     @Path("/{id}")
-    public Response getUser(@PathParam("id") Long id) {
+    public Response getUser(@PathParam("id") Long id) {        
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("user", new DAOImpl().getUser(id));   
         List<hackaton.model.Task> task = new DAOImpl().getMyTasks(id);               
@@ -32,6 +33,7 @@ public class User {
         return Response.ok(new Viewable("/user", map)).build();
 
     }
+  
     
     @POST   
     public Response postUser(@FormParam("first_name") String jmeno,@FormParam("last_name") String prijmeni,@FormParam("email") String email,@FormParam("comment") String comment){
@@ -42,5 +44,28 @@ public class User {
         map.put("user", u);
         return Response.ok(new Viewable("/user", map)).build();
        
+    }
+    
+    @POST
+    @Path("/{id}")
+    public Response putUser(@PathParam("id") Long id,@FormParam("first_name") String jmeno,@FormParam("last_name") String prijmeni,@FormParam("email") String email,@FormParam("comment") String comment){
+        Map<String, Object> map = new HashMap<String, Object>();
+        hackaton.model.User u = new DAOImpl().getUser(id);
+        
+        u.setFirstname(jmeno);
+        u.setSurname(prijmeni);
+        u.setEmail(email);
+        u.setComment(comment);
+        new DAOImpl().newUser(u);
+        map.put("user", u);
+         List<hackaton.model.Task> task = new DAOImpl().getMyTasks(id);               
+        List<TaskOverview> l = new ArrayList<TaskOverview>(); 
+     //    l.add( new TaskOverview("Vyhrát HackatOn", "Dlouhy popis", "obchodní", "probíhá", "#", 44));
+        for(hackaton.model.Task t : task){
+            l.add(new TaskOverview(t.getTitle(),t.getDescription(), t.getTypeName(), t.getStateName(), "/rest/task/"+t.getId(),t.getProgress()));
+        }
+        map.put("tasks", l);
+        return Response.ok(new Viewable("/user", map)).build();
+        
     }
 }
